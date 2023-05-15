cd2ec166931455d159ecbab29e48367b4ef0b5f7
litthy <litthy@qq.com>
2023-05-15 14:12:31 +0800

    fix: detail

--- src/components/manufacture/Recipe.vue
+++ src/components/manufacture/Recipe.vue
@@ -1,243 +1,196 @@
 <template>
-    <div class="m-manufacture-recipe" v-loading="loading" v-if="item">
-        <!-- 配方信息展示 -->
-        <el-popover popper-class="u-icon-popper" placement="right" :visible-arrow="false" trigger="hover">
-            <Item :item_id="item.item_key" />
-            <div class="u-img" slot="reference">
-                <span class="u-amount" v-if="item.CreateItemMin1 != item.CreateItemMax1"
-                    >{{ item.CreateItemMin1 }}~{{ item.CreateItemMax1 }}</span
+    <div class="m-manufacture-recipe">
+        <div class="m-recipe-list">
+            <span class="m-recipe-group" v-for="(item, i) in list" :key="i">
+                <span class="m-list m-title" @click="changeIndex(i)">{{ item.BelongName }}</span>
+                <span
+                    v-show="showIndex == i"
+                    class="m-list"
+                    v-for="(_list, k) in item.list"
+                    :key="k"
+                    @click="changeItem(_list.ID)"
                 >
-                <div
-                    class="u-border"
-                    :style="{ backgroundImage: item_border(item.Quality), opacity: item.Quality == 5 ? 0.9 : 1 }"
-                ></div>
-                <img :src="iconLink(item.IconID)" :alt="item.Name" />
-            </div>
-        </el-popover>
-
-        <span class="u-name" :class="`u-quality--${item.Quality}`">{{ item.Name }}</span>
-        <div class="u-price u-interval" v-if="client == 'std'">
-            [{{ server }}] 昨日平均价格:
-            <GamePrice v-if="price[item.item_key]" class="u-price-num" :price="price[item.item_key]" /><span
-                class="u-null"
-                v-else
-                >暂无数据</span
-            >
+                    <img class="u-img" :src="iconLink(_list.IconID)" :alt="_list.Name" />
+                    {{ _list.Name }}
+                </span>
+            </span>
         </div>
-        <div class="u-info u-interval">
-            <span
-                >需求等级: <b>{{ item.nLevel || "未知" }}</b></span
-            >
-            <span
-                >消耗精力: <b>{{ item.CostVigor || item.CostStamina }}</b></span
-            >
-            <span
-                >经验值: <b>{{ item.Exp }}</b></span
-            >
-        </div>
-        <template v-if="item.szTip">
-            <span class="u-desc" v-for="text in textFilter(item.szTip)" :key="text">{{ text }}</span>
-        </template>
-        <div class="u-children" v-if="childrenList && childrenList.length">
-            <el-divider content-position="left">合成所需材料</el-divider>
-            <div class="u-child u-interval" v-for="(el, index) in childrenList" :key="index">
-                <el-popover
-                    popper-class="u-icon-popper"
-                    placement="right"
-                    :visible-arrow="false"
-                    width="auto"
-                    trigger="hover"
-                >
-                    <Item :item_id="el.price_id" />
-                    <div class="u-img" slot="reference">
-                        <div
-                            class="u-border"
-                            :style="{ backgroundImage: item_border(el.Quality), opacity: el.Quality == 5 ? 0.9 : 1 }"
-                        ></div>
-                        <img v-if="el.item_info" :src="iconLink(el.item_info.IconID)" :alt="item.Name" />
-                    </div>
-                </el-popover>
-
-                <div class="u-info">
-                    <div class="u-name u-interval">
-                        <span v-if="el.item_info" :class="`u-quality--${el.Quality}`"> {{ el.item_info.Name }}</span>
-                        <span class="u-num">数量：x {{ el.count }}</span>
-                    </div>
-                    <div class="u-price" v-if="client == 'std'">
-                        {{ el.game_price ? "[NPC出售] 单价：" : `[${server}] 昨日平均单价：` }}
-                        <PriceItem
-                            :data="{
-                                Price: priceData[el.ID] || el.Price || price[el.price_id],
-                                Name: el.Name,
-                                id: el.ID,
-                            }"
-                        />
-                    </div>
-                    <div class="u-price" v-else>
-                        [NPC出售] 单价：
-                        <PriceItem :data="{ Price: priceData[el.ID] || el.Price, Name: el.Name, id: el.ID }" />
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="m-add">
-            <el-input-number v-model="count" :min="1" @click.stop.native></el-input-number>
-            <el-button icon="el-icon-shopping-cart-2" type="success" @click="addCart"> </el-button>
+        <div class="m-recipe-detail" v-loading="loading">
+            <RecipeDetail :showItem="showItem" :children="children" :prices="prices" :server="server" />
         </div>
     </div>
 </template>
 <script>
-import { getManufactureItem, getAuctionPrice } from "@/service/manufacture";
+import { getManufactureItem, getAuctionPrice, getOther, getItemsPrice } from "@/service/manufacture";
 import { iconLink } from "@jx3box/jx3box-common/js/utils.js";
 import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
-import { mapState } from "vuex";
-import GamePrice from "@jx3box/jx3box-common-ui/src/wiki/GamePrice.vue";
-import Item from "@jx3box/jx3box-editor/src/Item.vue";
-import PriceItem from "@/components/manufacture/PriceItem.vue";
+import RecipeDetail from "@/components/manufacture/RecipeDetail.vue";
 export default {
     name: "Recipe",
-    components: { Item, PriceItem, GamePrice },
+    props: ["list", "craftKey", "server"],
     data: function () {
         return {
+            showIndex: 0,
+            showItem: {},
             loading: false,
-
-            count: "",
-            item: {},
-            price: {},
+            prices: {},
+            children: [],
         };
     },
+    components: { RecipeDetail },
     computed: {
-        ...mapState([
-            "item_id",
-            "client",
-            "server",
-            "craft_key",
-            "priceData",
-            "itemData",
-            "cartList",
-            "childrenList",
-            "hasItems",
-        ]),
+        client() {
+            return this.$store.state.client;
+        },
     },
     methods: {
         iconLink,
-        // // 数据加载
-        // // ================
-        // // 获取item数据
-        loadData() {
+        loadItem(id) {
             this.loading = true;
-
-            this.item_id &&
-                getManufactureItem(this.craft_key, this.item_id, this.client)
-                    .then((res) => {
-                        res.data && this.processor(res.data);
-                    })
-                    .finally(() => {
-                        this.loading = false;
+            getManufactureItem(this.craftKey, id, this.client)
+                .then((res) => {
+                    let _data = {},
+                        _child = [],
+                        _type = [],
+                        _count = [];
+                    // 处理数据：删除空数据 合并itemKey 提取材料id 和数量
+                    Object.keys(res.data).forEach((key) => {
+                        if (res.data[key]) {
+                            _data[key] = res.data[key];
+                            if (_data.CreateItemType1)
+                                _data["itemKey"] = _data.CreateItemType1 + "_" + _data.CreateItemIndex1;
+                            if (key.startsWith("RequireItemIndex")) _child.push(_data[key]);
+                            if (key.startsWith("RequireItemType")) _type.push(_data[key]);
+                            if (key.startsWith("RequireItemCount")) _count.push(_data[key]);
+                        }
                     });
+                    // 材料id和数量处理
+                    _child = _child.map((id, i) => {
+                        return { id: id, count: _count[i], price_id: _type[i] + "_" + id };
+                    });
+                    // 获取材料详情
+                    this.getItemDetail(_child).then((res) => {
+                        this.children = _child.map((item) => {
+                            if (res[item.id]) item = Object.assign(item, res[item.id]);
+                            return item;
+                        });
+                    });
+                    this.showItem = _data;
+                    // 获取配方物品交易行价格
+                    this.getTradePrice(_child, _data.itemKey);
+                    this.getItemPrice(_child);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
         },
-        // 1、过滤空值 拼合物品ID
-        processor(data) {
-            let _obj = {},
-                _type = [],
-                _list = [],
-                _count = [],
-                _item = [];
-            Object.keys(data).forEach((key) => {
-                if (data[key]) {
-                    if (key.startsWith("RequireItem")) {
-                        if (key.startsWith("RequireItemType")) _type.push(data[key]);
-                        if (key.startsWith("RequireItemIndex")) _list.push(data[key]);
-                        if (key.startsWith("RequireItemCount")) _count.push(data[key]);
-                    } else if (key.startsWith("CreateItem")) {
-                        if (key.startsWith("CreateItemType1")) _item[0] = data[key];
-                        if (key.startsWith("CreateItemIndex1")) _item[1] = data[key];
-                        if (key.startsWith("CreateItemMax1")) _obj[key] = data[key];
-                        if (key.startsWith("CreateItemMin1")) _obj[key] = data[key];
-                    } else {
-                        _obj[key] = data[key];
-                    }
-                }
-            });
-            _list = _list.map((str, i) => {
-                str = { id: str, count: _count[i], price_id: _type[i] + "_" + str };
-                return str;
-            });
-            _obj.item_key = _item[0] + "_" + _item[1];
-            _obj.count = 1;
-            const childrenList = _list.length ? _list : [];
-            this.$store.commit("toState", { childrenList });
-            this.$store.dispatch("asyncChildrenList", childrenList);
-            this.toAuctionPrice(childrenList, _obj.item_key);
-
-            this.item = _obj;
-            this.$store.commit("toState", { item: this.item });
+        // 获取物品数据
+        async getItemDetail(arr) {
+            const ids = arr.map((item) => item.id).join();
+            try {
+                return await getOther({ client: this.client, ids, per: ids.length }).then((res) => {
+                    let itemData = {};
+                    res.data.list.forEach((item) => {
+                        let _obj = {};
+                        Object.keys(item).forEach((el) => {
+                            if (item[el]) _obj[el] = item[el];
+                        });
+                        itemData[item.ID] = { ..._obj, ..._obj.item_info };
+                        delete itemData[item.ID].Price;
+                        delete itemData[item.ID].item_info;
+                    });
+                    return itemData;
+                });
+            } catch (e) {
+                console.error("异常被捕获:", e);
+            }
         },
-        // 获取交易行价格
-        toAuctionPrice(arr = this.childrenList, key = this.item.item_key) {
-            let price_ids = arr.map((item) => item.price_id);
-            getAuctionPrice({ itemIds: price_ids.concat(key).join(), server: this.server }).then((res) => {
+        // NPC出售价格
+        async getItemPrice(arr) {
+            const ids = arr.map((item) => item.id).join();
+            getItemsPrice({ ids, client: this.client }).then((res) => {
+                let priceData = {};
+                res.data.forEach((item) => {
+                    priceData[item.ItemIndex] = item.Price;
+                });
+                this.prices = Object.assign(this.prices, priceData);
+            });
+        },
+        // 交易行价格
+        async getTradePrice(arr, key) {
+            const itemIds = arr
+                .map((item) => item.price_id)
+                .concat(key)
+                .join();
+            getAuctionPrice({ itemIds, server: this.server }).then((res) => {
                 let priceData = {};
                 Object.keys(res.data.data).forEach((item) => {
                     priceData[item] = res.data.data[item].AvgPrice;
                 });
-                this.price = priceData;
-                this.$store.commit("toState", { auctionPrice: priceData });
+                this.prices = Object.assign(this.prices, priceData);
             });
         },
-        // 添加到购物车
-        addCart() {
-            this.$store.dispatch("toAddCart", { count: this.count });
+        // 切换分类
+        changeIndex(i) {
+            this.showIndex = this.showIndex == i ? -1 : i;
         },
-
-        // 描述过滤
-        textFilter(str) {
-            // 匹配分段
-            const regex = /<Text>text="(.*?)"\s*?font=(\d+)\s*?<\/text>/gimsy;
-            let matches = [];
-            let match;
-            while ((match = regex.exec(str))) {
-                matches.push(match);
-            }
-
-            // 格式化分段
-            let result = [];
-            for (let group of matches) {
-                result.push(group[1].replace(/[\\n]/g, ""));
-            }
-            result = result.filter(Boolean);
-            return result;
-        },
-
-        // icon边框
-        item_border(id) {
-            switch (id) {
-                case 3:
-                    return `url(${__imgPath}image/item/blue.png)`;
-                case 4:
-                    return `url(${__imgPath}image/item/purple.png)`;
-                case 5:
-                    return `url(${__imgPath}image/item/orange.gif)`;
-                default:
-                    return "";
-            }
+        // 切换物品
+        changeItem(id) {
+            this.loadItem(id);
         },
     },
     watch: {
-        item_id() {
-            this.loadData();
-        },
-        server() {
-            this.toAuctionPrice();
-        },
-        count(count) {
-            this.item.count = count;
-            this.$store.commit("toState", { item: this.item });
+        list: {
+            deep: true,
+            handler: function (_list) {
+                if (_list.length) this.changeItem(_list[0].list[0].ID);
+            },
         },
     },
-    created: function () {},
 };
 </script>
 <style lang="less">
-@import "~@/assets/css/manufacture/recipe.less";
+.m-manufacture-recipe {
+    .flex;
+    gap: 20px;
+    .m-recipe-list {
+        .w(460px);
+        .flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+    .m-recipe-group {
+        .flex;
+        .color(#000);
+        flex-direction: column;
+        gap: 10px;
+        .m-list {
+            .flex;
+            .size(100%,50px);
+            .r(10px);
+            .pointer;
+            .fz(14px);
+            box-sizing: border-box;
+            gap: 10px;
+            font-weight: 700;
+            background-color: #fff;
+            padding: 0 20px;
+            align-items: center;
+            &.m-title {
+                .h(38px);
+                .fz(16px);
+            }
+            .u-img {
+                .size(20px);
+            }
+        }
+    }
+    .m-recipe-detail {
+        .w(460px);
+        .r(10px);
+        padding: 20px;
+        box-sizing: border-box;
+        background-color: #fff;
+    }
+}
 </style>
--- src/views/pvg/Manufacture.vue
+++ src/views/pvg/Manufacture.vue
@@ -1,153 +1,135 @@
 <template>
-    <div class="m-manufacture-container">
+    <div class="m-manufacture">
         <div class="m-manufacture-header">
-            <h1 class="u-title">技艺助手</h1>
-            <div class="u-settings" v-if="isStd">
-                <div class="u-servers">
-                    <el-select v-model="server" placeholder="请选择" size="small">
-                        <span slot="prefix" class="u-prefix"><i class="el-icon-coin"></i> 价格参考服务器</span>
-                        <el-option v-for="item in server_list" :key="item" :label="item" :value="item"> </el-option>
-                    </el-select>
-                </div>
-            </div>
+            <span v-for="(item, i) in craftList" :key="i" @click="changeCraft(i)">{{ item.name }}</span>
+            <el-input class="u-input" v-model.lazy="search" :placeholder="`搜索${craftName}配方`">
+                <el-button slot="prepend" icon="el-icon-search"></el-button>
+            </el-input>
         </div>
-        <div class="m-manufacture-main">
-            <div class="m-manufacture-sidebar">
-                <!-- 左侧 & 可制作配方 -->
-                <Make class="u-left" />
+        <div class="m-manufacture-body">
+            <!-- 配方 -->
+            <div class="m-manufacture-box">
+                <div class="m-manufacture-server">{{ server }}</div>
+                <Recipe :list="showList" :craftKey="craftKey" :server="server" />
             </div>
-            <div class="m-manufacture-content">
-                <!-- 中间 & 配方展示 -->
-                <Recipe class="u-middle" />
-                <!-- 右侧 & 购物车计算 -->
-                <Cart class="u-right" />
-            </div>
+
+            <!-- 成本计算器 -->
+            <!-- 我的清单 -->
         </div>
     </div>
 </template>
 
 <script>
+import { getCraftJson, getManufactures } from "@/service/manufacture";
 import servers_std from "@jx3box/jx3box-data/data/server/server_std.json";
 import servers_origin from "@jx3box/jx3box-data/data/server/server_origin.json";
-import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+import { craft_types } from "@/assets/data/manufacture.json";
+// import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
 
 import Recipe from "@/components/manufacture/Recipe.vue";
-import Make from "@/components/manufacture/Make.vue";
-import Cart from "@/components/manufacture/Cart.vue";
+// import Make from "@/components/manufacture/Make.vue";
+// import Cart from "@/components/manufacture/Cart.vue";
 
 export default {
     name: "Manufacture",
-    components: { Make, Recipe, Cart },
+    components: { Recipe },
     data: function () {
         return {
+            craftList: [],
+            search: "",
+            craftName: "",
+            index: -1,
+            showList: [],
+            craftKey: "",
+            craftTypes: craft_types,
             server: "蝶恋花",
-            craft_key: "",
-            item_id: "",
-            count: 0,
-            cart_list: [],
-            cache_data: {},
-            my_prices: {},
+            // craft_key: "",
+            // item_id: "",
+            // count: 0,
+            // cart_list: [],
+            // cache_data: {},
+            // my_prices: {},
         };
     },
     computed: {
-        // 服务器
-        server_list() {
-            return this.client == "std" ? servers_std : servers_origin;
-        },
         client() {
             return this.$store.state.client;
         },
         isStd() {
             return this.client == "std";
         },
-        item() {
-            return this.cacheItemData(this.cache_data[this.server][this.item_id]);
+        serverList() {
+            return this.isStd ? servers_std : servers_origin;
         },
-        // recipe_props() {
-        //     let _data = {
-        //         client: this.client,
-        //         item_id: this.item_id,
-        //         server: this.server,
-        //         craft_key: this.craft_key,
-        //         my_prices: this.my_prices,
-        //     };
-        //     if (this.item) _data.item = this.item;
-        //     if (this.count) _data.count = this.count;
-        //     return _data;
-        // },
-        // cart_props() {
-        //     return {
-        //         list: this.cart_list,
-        //         server: this.server,
-        //         my_prices: this.my_prices,
-        //     };
-        // },
     },
     methods: {
-        // 子组件传值
-        // isEmit(data) {
-        //     let { id, count, craft_key, my_price, item, del } = data;
-        //     this.item_id = id || this.item_id;
-        //     this.count = count || 0;
-        //     if (craft_key) this.craft_key = craft_key;
-        //     if (my_price) this.cachePrice(my_price);
-        //     if (item) this.cache_data[this.server][item.ID] = item;
-        //     count && item ? this.addToCart(item, count) : "";
-        //     if (del) {
-        //         del == -1
-        //             ? this.cart_list.splice(0, this.cart_list.length)
-        //             : (this.cart_list = this.cart_list.filter((item) => item.ID !== del));
-        //     }
-        // },
-        // // 加入购物车
-        // addToCart(obj, count) {
-        //     this.cart_list.some((item) => item.ID == obj.ID)
-        //         ? this.cart_list.forEach((item) => {
-        //               if (item.ID == obj.ID) {
-        //                   item.children = obj.children;
-        //                   item.item.count += count;
-        //               }
-        //           })
-        //         : this.cart_list.push(obj);
-        //     this.count = 0;
-        // },
-        // // 更新已获取过的物品
-        // cacheItemData(item) {
-        //     if (!item) return;
-        //     let data = this.my_prices;
-        //     if (data[item.ID]) item.Price = data[item.ID].Price;
-        //     item.children = item.children.map((el) => {
-        //         if (data[el.id]) el.Price = data[el.id].Price;
-        //         return el;
-        //     });
-        //     return item;
-        // },
-        // // 更新物品价格
-        // cachePrice(my_price) {
-        //     this.my_prices[my_price.id] = my_price;
-        //     let _prices = this.my_prices;
-        //     if (this.cart_list.length)
-        //         this.cart_list = this.cart_list.map((item) => {
-        //             if (_prices[item.ID]) item.Price = _prices[item.ID].Price;
-        //             item.children.map((el) => {
-        //                 if (_prices[el.id]) el.Price = _prices[el.id].Price;
-        //                 return el;
-        //             });
-        //             return item;
-        //         });
-        // },
-    },
-    watch: {
-        server: {
-            immediate: true,
-            handler: function (val) {
-                this.$store.commit("toState", { server: val });
-            },
+        // 获取数据
+        load() {
+            getCraftJson()
+                .then((res) => {
+                    const { std, origin } = res.data;
+                    this.craftList = this.client == "std" ? this.toCraftList(std) : this.toCraftList(origin);
+                    if (this.craftList.length) this.index = 0;
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
         },
+        // 获取配方列表 && 合并入配方分类
+        loadList(type, index) {
+            getManufactures({ client: this.client, type, mode: "simple" }).then((res) => {
+                // 配方分类
+                const list = this.craftList[index].list;
+                // 配方数据进行分组
+                const data = res.data.reduce((acc, cur) => {
+                    acc[cur.Belong] ? acc[cur.Belong].push(cur) : (acc[cur.Belong] = [cur]);
+                    return acc;
+                }, {});
+
+                // 合并数据配方分类
+                this.showList = list
+                    .map((item) => {
+                        if (data[item.BelongID]) item.list = data[item.BelongID];
+                        return item;
+                    })
+                    .filter((item) => item.list);
+                this.craftKey = type;
+            });
+        },
+        // 处理技艺列表
+        toCraftList(data) {
+            const type = this.craftTypes[this.client];
+            // 1. 将列表按ProfessionID分类
+            const craftData = data.reduce((acc, cur) => {
+                acc[cur.ProfessionID] ? acc[cur.ProfessionID].push(cur) : (acc[cur.ProfessionID] = [cur]);
+                return acc;
+            }, {});
+
+            // 2.为分类添加name和key值
+            return type.map((item) => {
+                if (craftData[item.ProfessionID]) item.list = craftData[item.ProfessionID];
+                return item;
+            });
+        },
+        // 切换技艺
+        changeCraft(i) {
+            this.index = i;
+        },
+    },
+
+    watch: {
+        index(i) {
+            const { name, key } = this.craftList[i];
+            this.craftName = name;
+            this.loadList(key, i);
+        },
+    },
+    mounted() {
+        this.load();
     },
 };
 </script>
 
 <style lang="less">
-@import "~@/assets/css/manufacture/index.less";
+@import "~@/assets/css/manufacture/new_index.less";
 </style>
