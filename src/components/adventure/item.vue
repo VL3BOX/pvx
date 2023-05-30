6c9f72d3d51f94b07b54f15c7919d63ddd67dda7
X3ZvaWQ <x3zvawq@gmail.com>
2023-05-30 11:20:39 +0800

    feat: 奇遇列表触发图片允许切换阵营/门派预览

--- src/components/adventure/item.vue
+++ src/components/adventure/item.vue
@@ -3,21 +3,42 @@
         <!-- @click="getLink(item.dwID)" -->
         <img class="u-default" :src="defaultImg" />
         <img class="u-title" :src="titleImg" :style="titleStyle" />
-        <span class="u-img" :style="`backgroundImage: url(${imgName})`"></span>
+        <span class="u-img" :style="`backgroundImage: url(${getImgUrl()})`"></span>
         <span class="u-icon"></span>
+        <div v-if="item.szRewardType === 'camp'" class="u-camp-switch" @click.prevent="switchCamp">
+            <img v-if="camp === 1" class="u-camp-icon" src="@/assets/img/camp/camp_1.png" />
+            <img v-if="camp === 2" class="u-camp-icon" src="@/assets/img/camp/camp_2.png" />
+        </div>
+        <div v-if="item.szRewardType === 'school'" class="u-school-switch" @click.prevent="switchCamp">
+            <el-popover ref="schoolPopover" placement="bottom" width="180" trigger="click" popper-class="m-school-choose">
+                <img slot="reference" class="u-school-icon" :src="forceIconUrl(force)" />
+                <div class="u-school-list">
+                    <img
+                        v-for="(name, id) in forceid"
+                        :key="id"
+                        class="u-school-item"
+                        :src="forceIconUrl(id)"
+                        @click="switchSchool(id)"
+                    />
+                </div>
+            </el-popover>
+        </div>
     </a>
 </template>
 
 <script>
 import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+import forceid from "@jx3box/jx3box-data/data/xf/forceid.json";
 export default {
     name: "item",
     props: ["item"],
     inject: ["__imgRoot"],
     components: {},
-    data: function () {
-        return {};
-    },
+    data: () => ({
+        forceid,
+        camp: 1,
+        force: 2,
+    }),
     computed: {
         link: function () {
             return "/adventure/" + this.item.dwID;
@@ -42,8 +63,35 @@ export default {
         },
     },
     methods: {
+        forceIconUrl(force) {
+            const forceName = forceid[force];
+            return `${__imgPath}image/school/${forceName}.png`;
+        },
+        getImgUrl() {
+            let tgaPath = this.item.szOpenRewardPath?.toLowerCase();
+            if (!tgaPath) return "";
+            tgaPath = tgaPath.replace(/\\/g, "/").replace("ui/image/adventure/", "");
+            if (!this.item.szRewardType) {
+                let pngPath = tgaPath.replace(/\.tga$/, ".png");
+                return `${this.__imgRoot}adventure/${this.client}/${pngPath}`;
+            }
+            // 传给组件的数据是修改过的
+            tgaPath = tgaPath.replace(/\/[^\/]+?\.tga$/, "");
+            if (this.item.szRewardType === "camp")
+                return `${this.__imgRoot}adventure/${this.client}/${tgaPath}/camp_${this.camp}_open.png`;
+            if (this.item.szRewardType === "school")
+                return `${this.__imgRoot}adventure/${this.client}/${tgaPath}/school_${this.force}_open.png`;
+            return defaultImg;
+        },
         getLink(adventure_id) {
             this.$router.push({ name: "single", params: { id: adventure_id } });
+        },
+        switchCamp() {
+            this.camp = this.camp === 1 ? 2 : 1;
+        },
+        switchSchool(force) {
+            this.force = force;
+            this.$refs.schoolPopover.doClose();
         },
     },
     filters: {},
