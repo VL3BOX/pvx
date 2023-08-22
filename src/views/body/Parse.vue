a13dab50213cb73e5a5fc17670317f7dfdd98c6d
litthy <litthy@qq.com>
2023-08-22 15:55:22 +0800

    feat: Parse

--- src/views/body/Parse.vue
+++ src/views/body/Parse.vue
@@ -1,16 +1,47 @@
 <template>
-    <div class="m-body-parse" :class="{ on: done }">
-        <h1 class="m-body-parse-title">体型数据解析器</h1>
+    <div class="m-facedata">
+        <div class="m-face-parse" :class="{ on: done }">
+            <h1 class="m-face-parse-title">体型数据解析器</h1>
+            <Upload type="pvxbody" @success="handleSuccess" />
+            <Bodydat class="m-face-parse-preview" :data="json" :lock="false" v-show="done" />
+        </div>
     </div>
 </template>
 
 <script>
+import Bodydat from "@jx3box/jx3box-facedat/src/Bodydat";
+import Upload from "@jx3box/jx3box-facedat/src/Upload";
+import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
 export default {
     name: "BodyDataParse",
     data: function () {
-        return {};
+        return {
+            data: "",
+            done: false,
+        };
     },
-    computed: {},
-    methods: {},
+    computed: {
+        json: function () {
+            return this.data && this.data.json;
+        },
+    },
+    methods: {
+        handleSuccess: function (data) {
+            this.data = data;
+            if (data?.json) {
+                this.done = true;
+            } else {
+                this.done = false;
+            }
+        },
+    },
+    components: {
+        Bodydat,
+        Upload,
+    },
 };
 </script>
+
+<style lang="less">
+@import "~@/assets/css/face/parse.less";
+</style>
