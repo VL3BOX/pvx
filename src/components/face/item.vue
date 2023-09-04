fbf41efc5be6f6643c03f7f7f45e905a80314250
litthy <litthy@qq.com>
2023-09-04 10:29:52 +0800

    feat: 可新建

--- src/components/face/item.vue
+++ src/components/face/item.vue
@@ -7,7 +7,11 @@
     >
         <div class="m-img">
             <img class="u-pic" :src="showThumb(imgLink)" loading="lazy" />
-            <i class="u-mark u-mark--star" v-if="!!item.star">编辑推荐</i>
+            <div class="m-mark-left">
+                <i class="u-mark u-mark--star" v-if="!!item.star">编辑推荐</i>
+                <i class="u-mark u-mark--new" v-if="!!item.is_unlimited">可新建</i>
+            </div>
+
             <i class="u-mark u-mark--pay" v-if="!!~~item.price_type && !!item.price_count">付费</i>
         </div>
 
