3b26a1fd2390e3e3d742587ffbd4c52e628d93b0
fifththirteen <570205219@qq.com>
2024-03-25 15:04:23 +0800

    style:家园声望

--- src/components/gonggao/daily/Reputation.vue
+++ src/components/gonggao/daily/Reputation.vue
@@ -117,12 +117,15 @@ export default {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     margin-bottom: 10px;
+    overflow-y: auto;
+    .scrollbar();
 
     .u-reputation-item {
         .flex;
         align-items: center;
         justify-content: space-between;
         padding: 2px 5px;
+        gap: 5px;
         &:hover {
             background: @bg-gray;
             .u-reputation-name {
